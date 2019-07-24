b66a0aef2e1f0f6dde21aa6c05c547d04f0cc7e0
Kibo Hikari <enra@sayonika.moe>
2019-07-24 20:05:01 +0800

    [setup] format and remove unused setting
    Signed-off-by: Kibo Hikari <enra@sayonika.moe>

--- studio.sh
+++ studio.sh
@@ -14,21 +14,21 @@ XORG_LOG="/var/log/Xorg.0.log"
 VERSION_NUM="1.20.0"
 VERSION_FILE="${WINEPREFIX}/com.invisionapp.Studio.version"
 
-WINE_PACKAGES=(directx9 usp10 msls31 corefonts tahoma dotnet452 vcrun2015 win10)
+WINE_PACKAGES="directx9 usp10 msls31 corefonts tahoma dotnet452 vcrun2015 win10"
 
 echo "########################################################"
 echo "## Invision Studio Unofficial Flatpak v${VERSION_NUM} ##"
 echo "########################################################"
 echo
 
-set_wine_settings(){
-  local my_documents="${WINEPREFIX}/drive_c/users/${USER}/My Documents"
+set_wine_settings() {
+  my_documents="${WINEPREFIX}/drive_c/users/${USER}/My Documents"
 
   echo "WORKAROUND: set registry for dotnet452"
   wine reg add "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Component Based Servicing\\Version" /v 6.1.7601.17592 /t REG_SZ /d "foo" /f
 
   echo "Installing wine requirements."
-  winetricks --unattended "${WINE_PACKAGES[@]}"
+  winetricks --unattended "${WINE_PACKAGES}"
 
   # Symlink points to wrong location, fix it
   if [[ "$(readlink "${my_documents}")" != "${XDG_DOCUMENTS_DIR}" ]]; then
@@ -41,20 +41,20 @@ set_wine_settings(){
 }
 
 # Run only if Invision Studio isn't installed
-first_run(){
+first_run() {
   set_wine_settings
 
   echo "${VERSION_NUM}" > "${VERSION_FILE}"
 
   if [ ! -f "${INVISON_SETUP}" ]; then
     echo "Downloading Invision Studio installer."
-    wget -O ${INVISION_SETUP} ${INVISION_DL_URL}
+    wget --output-document=${INVISION_SETUP} ${INVISION_DL_URL}
   fi
   echo "Running Invision Studio installer."
   "${WINE}" "${INVISION_SETUP}"
 }
 
-is_updated(){
+is_updated() {
   if [ -f "${VERSION_FILE}" ]; then
     last_version="$(cat ${VERSION_FILE})"
   else
@@ -71,7 +71,7 @@ is_updated(){
 }
 
 # Main function
-startup(){
+startup() {
 
   if [ ! -d "${WINEPREFIX}/drive_c/users/${USER}/AppData/Local/Invision Studio" ]; then
      echo "Ruh roh! Invision Studio is not installed."
@@ -82,11 +82,6 @@ startup(){
      set_wine_settings
      fi
  fi
-
-
-  echo "Setting resolution to ${WINE_RESOLUTION}"
-  echo "If resolution was changed from default, program may need restarting"
-  winetricks --unattended vd="${WINE_RESOLUTION}" >/dev/null
 
   echo ; echo "Starting Invision Studio..."
   "${WINE}" "${INVISION_RUN_CMD}"
